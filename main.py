e1a0322d88a62891191270e7f4902a91e7a13f83
programer6yrt5j <166051619+programer6yrt5j@users.noreply.github.com>
2024-04-11 20:46:37 +0300

    Update main.py
    add control rocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+SPEED = 4
 FPS = 60
 WIDTH = 700
 HEIGHT = 500
@@ -39,13 +40,38 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self) -> None:
+    def update_r(self) -> None:
         key_pressed = key.get_pressed()
-        if key_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if key_pressed[K_RIGHT] and self.rect.x < WIDTH - (self.width + 5):
-            self.rect.x += self.speed
+        if key_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key_pressed[K_DOWN] and self.rect.y < HEIGHT - (self.height + 5):
+            self.rect.y += self.speed
 
+    def update_l(self) -> None:
+        key_pressed = key.get_pressed()
+        if key_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key_pressed[K_s] and self.rect.y < HEIGHT - (self.height + 5):
+            self.rect.y += self.speed
+
+
+player_r = Player(
+    player_image='images/rocket.png',
+    x=615,
+    y=200,
+    speed=SPEED,
+    width=50,
+    height=150
+)
+
+player_l = Player(
+    player_image='images/rocket.png',
+    x=30,
+    y=200,
+    speed=SPEED,
+    width=50,
+    height=150
+)
 
 timer = time.Clock()
 finish = False
@@ -56,7 +82,13 @@ while game:
         if current_event.type == QUIT:
             game = False
 
+    player_l.update_l()
+    player_r.update_r()
+
     window.blit(back_fon, (0, 0))
+
+    player_l.reset()
+    player_r.reset()
 
     display.update()
     timer.tick(FPS)
